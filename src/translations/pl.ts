d12a5ae422203d0c51d7831d1909a1fdfc36fa24
MaciejDurski <maciej-durski1@wp.pl>
2023-04-06 18:53:26 +0200

    wip

--- src/translations/pl.ts
+++ src/translations/pl.ts
@@ -1,6 +1,21 @@
 export const translationsPL = {
   common: {
     guest: "Gość",
+    guests: "Goście",
+    nameAndSurrname: "Imię i nazwisko",
+    email: "E-mail",
+    phone: "Numer telefonu",
+    type: "Typ",
+    guardian: "Opiekun",
+    organizers: "Organizatorzy",
+  },
+  guest: {
+    checkIn: "Odprawa",
+    accommodationNote: "Notatka do noclego",
+    accommodation: "Nocleg",
+    ownComputer: "Własny komputer",
+    presentationLength: "Długość prezentacji",
+    specialRequirements: "Specjalne wymagania",
   },
   guestForm: {
     firstName: "Imię",
